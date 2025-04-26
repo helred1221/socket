f8c26759d9a7faa3974295da79a2c534253b1db5
helred1221 <helder.pereira@estudante.ifms.edu.br>
2025-04-26 17:48:56 -0400

    fix: changed class on server

--- servidor.ts
+++ servidor.ts
@@ -1,46 +1,60 @@
-import * as net from 'net';
+import * as net from "net";
 
 const porta: number = 3001;
 
-interface ProdutoInterface {
-  titulo: string,
-  preco: number,
-  descricao: string
+interface BancadaProps {
+  temperatura: number;
+  umidade: number;
+  condutividade: number;
+  port: number;
 }
 
-class Produto implements ProdutoInterface {
-  titulo: string = '';
-  preco: number = 0;
-  descricao: string = '';
+class Bancada implements BancadaProps {
+  temperatura: number = 0;
+  umidade: number = 0;
+  condutividade: number = 0;
+  port: number = 0;
 
-  constructor(titulo: string, preco: number, descricao: string){
-    this.titulo = titulo;
-    this.preco = preco;
-    this.descricao = descricao;
+  constructor(
+    temperatura: number,
+    umidade: number,
+    condutividade: number,
+    port: number
+  ) {
+    this.temperatura = temperatura;
+    this.umidade = umidade;
+    this.condutividade = condutividade;
+    this.port = port;
   }
 
-  calcularDesconto(porcentagem: number) {
-    return this.preco - ((this.preco * porcentagem) / 100);
+  getTemperatura(): number {
+    return this.temperatura;
   }
 
+  getUmidade(): number {
+    return this.umidade;
+  }
+
+  getCondutividade(): number {
+    return this.condutividade;
+  }
 }
 
 const servidor: net.Server = net.createServer((socket: net.Socket) => {
-  console.log('Cliente conectado');
+  console.log("Cliente conectado");
 
   socket.write("Bem-vindo ao servidor!\n");
   socket.write(`${socket.remotePort} - ${socket.remoteAddress}\n`);
 
-  socket.on('data', (mensagem: Buffer) => {
-    console.log(JSON.parse(mensagem.toString('utf-8')));
-  })
+  socket.on("data", (mensagem: Buffer) => {
+    console.log(JSON.parse(mensagem.toString("utf-8")));
+  });
 
-  socket.on('close', () => {
-    console.log('Cliente desconectado');
-  })
-})
+  socket.on("close", () => {
+    console.log("Cliente desconectado");
+  });
+});
 
 servidor.listen(porta, () => {
   console.log(`Servidor escutando na porta ${porta}`);
-})
-
+});
